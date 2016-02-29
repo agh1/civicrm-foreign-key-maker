5a803edfec0004aa90b92a127ef052e645009630
Andrew Hunt <andrew@aghstrategies.com>
2016-02-29 10:38:22 -0500

    As working 2-29-16

--- fkmaker.php
+++ fkmaker.php
@@ -2,34 +2,57 @@
 /**
  * @file
  * Generate "add constraint" foreign key statements from schema.
+ *
+ * To use, run the following command first:
+ *   grep -rl '<foreignKey' path-to-civicrm/civicrm/xml > civicrm-foreign-key-maker/fklist.txt
+ * Then, run this script:
+ *   php fkmaker.php > constraints.sql
  */
+
+$files = file_get_contents('fklist.txt');
+$files = explode("\n", $files);
+foreach ($files as $file) {
+  $file = trim($file);
+  if (!strlen($file)) {
+    continue;
+  }
+  $xmlstr = trim(file_get_contents($file));
+  $tableXML = simplexml_load_string($xmlstr);
+  $name = trim((string) $tableXML->name);
+  foreach ($tableXML->foreignKey as $foreignXML) {
+    $foreignKeys = array();
+    getForeignKey($foreignXML, $foreignKeys, $name);
+  }
+  foreach ($foreignKeys as $foreignKey) {
+    $row = "ALTER TABLE $name ADD CONSTRAINT {$foreignKey['uniqName']} FOREIGN KEY (`{$foreignKey['name']}`) REFERENCES `{$foreignKey['table']}`(`{$foreignKey['key']}`)";
+    if ($foreignKey['onDelete']) {
+      $row .= " ON DELETE {$foreignKey['onDelete']}";
+    }
+    $row .= ";\n";
+    echo $row;
+  }
+}
 
 /**
  * Process foreign key.
  *
  * Lifted from CRM_Core_CodeGen_Specification::getForeignKey.
  */
-function getForeignKey(&$foreignXML, &$fields, &$foreignKeys, &$currentTableName) {
+function getForeignKey(&$foreignXML, &$foreignKeys, &$currentTableName) {
   $name = trim((string ) $foreignXML->name);
 
-  /** need to make sure there is a field of type name */
-  if (!array_key_exists($name, $fields)) {
-    echo "foreign $name in $currentTableName does not have a field definition, ignoring\n";
-    return;
-  }
-
   /** need to check for existence of table and key **/
-  $table = trim($this->value('table', $foreignXML));
+  $table = trim((string) $foreignXML->table);
   $foreignKey = array(
     'name' => $name,
     'table' => $table,
     'uniqName' => "FK_{$currentTableName}_{$name}",
-    'key' => trim($this->value('key', $foreignXML)),
-    'import' => $this->value('import', $foreignXML, FALSE),
-    'export' => $this->value('import', $foreignXML, FALSE),
+    'key' => trim((string) $foreignXML->key),
+    'import' => empty($foreignXML->import) ? FALSE : $foreignXML->import,
+    'export' => empty($foreignXML->export) ? FALSE : $foreignXML->export,
     // we do this matching in a separate phase (resolveForeignKeys)
     'className' => NULL,
-    'onDelete' => $this->value('onDelete', $foreignXML, FALSE),
+    'onDelete' => empty($foreignXML->onDelete) ? FALSE : $foreignXML->onDelete,
   );
   $foreignKeys[$name] = &$foreignKey;
 }
